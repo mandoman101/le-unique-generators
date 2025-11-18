09efc9b679af3497b20bfda68fb9b74b63867442
Armando Tabarez <armando@mando-man.com>
2025-11-18 13:45:03 -0600

    added fixed relative slide

---
+++ js/lib/http.js
@@ -0,0 +1,28 @@
+﻿const http = require('http');
+const requestIp = require('request-ip');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+const server = http.createServer((req, res) => {
+    const clientIp = requestIp.getClientIp(req);
+    logger.info({
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+        body: req.body,
+        clientIp: clientIp
+    });
+
+    res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
